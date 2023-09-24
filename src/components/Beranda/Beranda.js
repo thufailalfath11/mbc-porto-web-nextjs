fa6b7abcf0f8225ca3a41f005f0a89835a089a32
thufailalfath11 <thufailalfath11@gmail.com>
2023-09-24 09:39:57 -0700

    benerin

--- src/components/Beranda/Beranda.js
+++ src/components/Beranda/Beranda.js
@@ -71,35 +71,25 @@ function Beranda() {
         </span>
       </div>
 
+      <section id="about">
       <div className={styles.about}>
-        {showAbout && (
-          <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <Image
-              src="/about-image.png"
-              width={500}
-              height={500}
-              data-aos="fade-right"
-            />
-            <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-              <h2
-                className="text-4xl font-bold mb-5 bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600"
-                data-aos="fade-up"
-              >
-                Tentang Kami
-              </h2>
-              <p className="text-base lg:text-lg" data-aos="fade-up">
-                I am a full stack web developer with a passion for creating
-                interactive and responsive web applications. I have experience
-                working with JavaScript, React, Redux, Node.js, Express,
-                PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner
-                and I am always looking to expand my knowledge and skill set. I
-                am a team player and I am excited to work with others to create
-                amazing applications.
-              </p>
-            </div>
-          </div>
-        )}
+  {showAbout && (
+    <div className="md:grid md:grid-cols-2 gap-8 justify-center items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16" data-aos="fade-up">
+      <Image src="/about-image.png" width={500} height={500} data-aos="fade-right" />
+      <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+        <h2 className="text-4xl font-bold mb-2 bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600" data-aos="fade-up">
+          Tentang Kami
+        </h2>
+        <p className="text-base lg:text-lg mb-4" data-aos="fade-up">
+          I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
+        </p>
       </div>
+    </div>
+  )}
+</div>
+
+      </section>
+      <section id="about">
       <div className={styles.about}>
         <div
           className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16"
@@ -124,6 +114,7 @@ function Beranda() {
           <Image src="/1.png" width={500} height={500} data-aos="fade-left" />
         </div>
       </div>
+      </section>
     </div>
   );
 }
