4160e09517e1f79c6047792715009c23acbae21a
thufailalfath11 <thufailalfath11@gmail.com>
2023-09-24 09:25:21 -0700

    benerin beranda

--- src/app/beranda/page.js
+++ src/app/beranda/page.js
@@ -14,13 +14,13 @@ const beranda = () => {
       <div className="w-full h-screen absolute top-0 left-0 z-0">
         <ParticklesBG />
       </div>
-      <div className="max-w-6xl mx-auto relative z-1">
+      
         <div className="container">
           {/* Navbar, Beranda, dan Footer berada di atas ParticklesBG */}
           <Navbar />
           <Beranda />
           <Footer />
-        </div>
+      
       </div>
     </div>
   );
