c1887d769f5f197c6466a396bd7df7bef3abb210
pandudwinugraha <pandudwinugraha01@gmail.com>
2023-09-25 18:38:28 +0700

    buat text

--- src/components/Tentang/items.js
+++ src/components/Tentang/items.js
@@ -1,30 +1,30 @@
 export const items = [
     {
       id: 1,
-      title: "cs",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      title: "Cyber Security",
+      desc: "Cybersecurity adalah salah satu devisi MBClab yang berfokus kepada praktik dan teknologi yang digunakan untuk melindungi sistem komputer dan jaringan dari ancaman siber, seperti peretasan, malware, atau pencurian data. Devisi ini memiliki tim yang mengkhususkan diri dalam menguji dan meningkatkan keamanan sistem serta merespons serangan siber untuk melindungi data dan infrastruktur yang berdedikasi untuk mengamankan data dan aplikasi berbasis cloud.",
+      image: "/CS.jpg",
     url: "/tentang/anggotacs"
     },
     {
       id: 2,
-      title: "bd",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      title: "Big Data",
+      desc: "Big Data adalah salah satu devisi MBClab yang berfokus kepada data yang melibatkan analisis dan pemahaman data yang sangat besar dan kompleks, yang mencakup penggunaan teknik Pembelajaran Mesin untuk mengidentifikasi pola, tren, dan wawasan penting. Devisi ini memanfaatkan teknologi mutakhir untuk menganalisis dan menggali informasi berharga dari volume data yang besar.",
+      image: "/BD.jpg",
       url: "/tentang/anggotabd"
     },
     {
       id: 3,
-      title: "gt",
-      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-      image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      title: "Game Tech",
+      desc: "Game Tech adalah salah satu devisi Multimedia MBClab yang berfokus kepada pengembangan permainan video dan perangkat hiburan interaktif lainnya. Laboratorium menggunakan teknologi canggih untuk menciptakan simulasi interaktif dan pengalaman bermain yang mendalam.",
+      image: "/GT.jpg",
       url: "/tentang/anggotagt"
     },
     {
         id: 4,
-        title: "gis",
-        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-        image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        title: "GIS",
+        desc: "Sistem Informasi Geografis (GIS) adalah salah satu devisi Multimedia MBClab yang berfokus kepada penggunaan teknologi yang digunakan untuk mengumpulkan, mengelola, dan menganalisis data geografis, yang dapat mencakup pemetaan wilayah, pemantauan lingkungan, dan aplikasi lain yang berkaitan dengan lokasi geografis.",
+        image: "/GIS.jpg",
         url: "/tentang/anggotagis"
     }
   ];
