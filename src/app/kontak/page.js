d430bdcf2a272b25c3cfd5709426a05a8626d84e
rezaaditria <rezaaditria1@gmail.com>
2023-09-27 01:08:51 +0700

    bang udah bang

--- src/app/kontak/page.js
+++ src/app/kontak/page.js
@@ -6,10 +6,10 @@ import Navbar from "@/components/Navbar/page";
 const kontak = () => {
   return (
     <div>
-      <div className="container">
-        {/* <div className="w-full h-screen absolute top-0 left-0 z-0">
+      <div className="w-full h-screen absolute top-0 left-0 z-0">
         <ParticklesBG />
-      </div> */}
+      </div>
+      <div className="container">
         <Navbar />
         <Kontak />
         <Footer />
