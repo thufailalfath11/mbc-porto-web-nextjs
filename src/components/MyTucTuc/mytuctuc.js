f94261d7b009424f2f207dc4ed463e96c3a71e99
pandudwinugraha <pandudwinugraha01@gmail.com>
2023-09-27 18:31:13 +0700

    ganti nama

--- src/components/MyTucTuc/mytuctuc.js
+++ src/components/MyTucTuc/mytuctuc.js
@@ -23,7 +23,7 @@ function MyTucTuc() {
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.content}>
-          <h1 className={styles.title}>MyTucTuc</h1>
+          <h1 className={styles.title}>MyTuc</h1>
           <p className={styles.desc}>Tuc-Tuc merupakan alat transportasi yang berada di dalam kampus yang dapat digunakan oleh mahasiswa/i Telkom University untuk memudahkan mobilisasi atau kegiatan dalam lingkungan kampus. Karena itu kami membuat aplikasi MyTuc. MyTuc merupakan aplikasi mobile apps berbasis AR yang dibuat untuk memudahkan mahasiswa/i Telkom University untuk memantau rute Tel-U Car (TUC). Dimana pada aplikasi ini terdapat menu untuk rute, jadwal, tentang, dan bantuan. Serta pada aplikasi ini juga disediakan fitur scan AR, dimana rute Tuc-Tuc akan ditampilkan secara 3D animasi agar lebih memudahkan mahasiswa/i Telkom University untuk memahami rute Tuc-tuc.
 Dalam pembuatan aplikasi ini, kami menggunakan Blender untuk 3D modelling bangunan-bangunan yang dilewati oleh TUC. Selain itu, kami juga menggunakan Figma untuk desain interface dari aplikasi MyTUC ini. Terakhir, kami menggunakan Game Engine Unity untuk mewujudkan aplikasi MyTUC ini agar dapat di gunakan di HP masing-masing.</p>
           <div className={styles.Button}>
