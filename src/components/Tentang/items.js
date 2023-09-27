257e94c5100e26287deb60cbf0194e303b9526c9
pandudwinugraha <pandudwinugraha01@gmail.com>
2023-09-27 16:08:10 +0700

    tipis-tipis untuk mabrother

--- src/components/Tentang/items.js
+++ src/components/Tentang/items.js
@@ -1,31 +1,82 @@
 export const items = [
     {
       id: 1,
-      title: "Cyber Security",
-      desc: "Cybersecurity adalah salah satu devisi MBClab yang berfokus kepada praktik dan teknologi yang digunakan untuk melindungi sistem komputer dan jaringan dari ancaman siber, seperti peretasan, malware, atau pencurian data. Devisi ini memiliki tim yang mengkhususkan diri dalam menguji dan meningkatkan keamanan sistem serta merespons serangan siber untuk melindungi data dan infrastruktur yang berdedikasi untuk mengamankan data dan aplikasi berbasis cloud.",
+      title: "Apa itu MBC?",
+      desc: "MBC memiliki kepanjangan Multimedia, Big Data, and Cyber Security, salah satu Laboratorium diatas naungan Kelompok Keahlian Nertworking, Cybernetics, dan Enginerering Management (KK NCM) yang mempelajari mengenai CyberSecurity, Bigdata, GIS, dan Gametech.",
       image: "/divisi/CS.jpg",
     url: "/tentang/anggotacs"
     },
     {
       id: 2,
-      title: "Big Data",
-      desc: "Big Data adalah salah satu devisi MBClab yang berfokus kepada data yang melibatkan analisis dan pemahaman data yang sangat besar dan kompleks, yang mencakup penggunaan teknik Pembelajaran Mesin untuk mengidentifikasi pola, tren, dan wawasan penting. Devisi ini memanfaatkan teknologi mutakhir untuk menganalisis dan menggali informasi berharga dari volume data yang besar.",
+      title: "Sejarah Singkat MBC",
+      desc: "Laboratorium MBC pada awalnya bernama Laboratorium Aplication and Security (A&S) yang berada dibawah naungan Kelompok Keahlian Jaringan dan Multimedia (KK JMM) dan memiliki 6 divisi, antara lain Intrusion Detection (IDS),Voice Over Internet Protocol (VOIP), Game Technology, Interactive Video (IV), Geographic Information System, dan Web. Kemudian, berubah nama menjadi Laboratorium MBC pada 4 Oktober 2019 dengan 4 Divisi yaitu: Cyber Security, Bigdata,Multimedia (GIS, Game tech). ", 
       image: "/divisi/BD.jpg",
       url: "/tentang/anggotabd"
     },
     {
       id: 3,
-      title: "Game Tech",
-      desc: "Game Tech adalah salah satu devisi Multimedia MBClab yang berfokus kepada pengembangan permainan video dan perangkat hiburan interaktif lainnya. Laboratorium menggunakan teknologi canggih untuk menciptakan simulasi interaktif dan pengalaman bermain yang mendalam.",
+      title: "Divisi MBC",
+      desc: "Saat ini Laboratorium MBC Memiliki 4 devisi fokus khusus Cyber Security, Bigdata, GIS, dan Gametech.",
       image: "/divisi/GT.jpg",
       url: "/tentang/anggotagt"
-    },
-    {
+    },           
+       {
+        id: 1,
+        title: "Cyber Security",
+        desc: "Cybersecurity adalah salah satu devisi MBClab yang berfokus kepada praktik dan teknologi yang digunakan untuk melindungi sistem komputer dan jaringan dari ancaman siber, seperti peretasan, malware, atau pencurian data. Devisi ini memiliki tim yang mengkhususkan diri dalam menguji dan meningkatkan keamanan sistem serta merespons serangan siber untuk melindungi data dan infrastruktur yang berdedikasi untuk mengamankan data dan aplikasi berbasis cloud.",
+        image: "/divisi/CS.jpg",
+        url: "/tentang/anggotacs"
+        },
+        {
+        id: 2,
+        title: "Big Data",
+        desc: "Big Data adalah salah satu devisi MBClab yang berfokus kepada data yang melibatkan analisis dan pemahaman data yang sangat besar dan kompleks, yang mencakup penggunaan teknik Pembelajaran Mesin untuk mengidentifikasi pola, tren, dan wawasan penting. Devisi ini memanfaatkan teknologi mutakhir untuk menganalisis dan menggali informasi berharga dari volume data yang besar.",
+        image: "/divisi/BD.jpg",
+        url: "/tentang/anggotabd"
+        },
+        {
+        id: 3,
+        title: "Game Tech",
+        desc: "Game Tech adalah salah satu devisi Multimedia MBClab yang berfokus kepada pengembangan permainan video dan perangkat hiburan interaktif lainnya. Laboratorium menggunakan teknologi canggih untuk menciptakan simulasi interaktif dan pengalaman bermain yang mendalam.",
+        image: "/divisi/GT.jpg",
+        url: "/tentang/anggotagt"
+        },
+        {
         id: 4,
         title: "GIS",
         desc: "Sistem Informasi Geografis (GIS) adalah salah satu devisi Multimedia MBClab yang berfokus kepada penggunaan teknologi yang digunakan untuk mengumpulkan, mengelola, dan menganalisis data geografis, yang dapat mencakup pemetaan wilayah, pemantauan lingkungan, dan aplikasi lain yang berkaitan dengan lokasi geografis.",
         image: "/divisi/GIS.jpg",
         url: "/tentang/anggotagis"
-    }
+       }
   ];
+  // export const items = [
+  //   {
+  //     id: 1,
+  //     title: "Cyber Security",
+  //     desc: "Cybersecurity adalah salah satu devisi MBClab yang berfokus kepada praktik dan teknologi yang digunakan untuk melindungi sistem komputer dan jaringan dari ancaman siber, seperti peretasan, malware, atau pencurian data. Devisi ini memiliki tim yang mengkhususkan diri dalam menguji dan meningkatkan keamanan sistem serta merespons serangan siber untuk melindungi data dan infrastruktur yang berdedikasi untuk mengamankan data dan aplikasi berbasis cloud.",
+  //     image: "/divisi/CS.jpg",
+  //   url: "/tentang/anggotacs"
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Big Data",
+  //     desc: "Big Data adalah salah satu devisi MBClab yang berfokus kepada data yang melibatkan analisis dan pemahaman data yang sangat besar dan kompleks, yang mencakup penggunaan teknik Pembelajaran Mesin untuk mengidentifikasi pola, tren, dan wawasan penting. Devisi ini memanfaatkan teknologi mutakhir untuk menganalisis dan menggali informasi berharga dari volume data yang besar.",
+  //     image: "/divisi/BD.jpg",
+  //     url: "/tentang/anggotabd"
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Game Tech",
+  //     desc: "Game Tech adalah salah satu devisi Multimedia MBClab yang berfokus kepada pengembangan permainan video dan perangkat hiburan interaktif lainnya. Laboratorium menggunakan teknologi canggih untuk menciptakan simulasi interaktif dan pengalaman bermain yang mendalam.",
+  //     image: "/divisi/GT.jpg",
+  //     url: "/tentang/anggotagt"
+  //   },
+  //   {
+  //       id: 4,
+  //       title: "GIS",
+  //       desc: "Sistem Informasi Geografis (GIS) adalah salah satu devisi Multimedia MBClab yang berfokus kepada penggunaan teknologi yang digunakan untuk mengumpulkan, mengelola, dan menganalisis data geografis, yang dapat mencakup pemetaan wilayah, pemantauan lingkungan, dan aplikasi lain yang berkaitan dengan lokasi geografis.",
+  //       image: "/divisi/GIS.jpg",
+  //       url: "/tentang/anggotagis"
+  //   }
+  // ];
   
